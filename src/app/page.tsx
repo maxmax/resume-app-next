bd6af31b7617f62ba186e485b71b7a1cff6612a8
maxmax <maxbakaiev@gmail.com>
2024-03-01 10:07:03 +0200

    home px

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import RedirectForm from '@/ui/components/forms/redirect-form';
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <div className="w-full max-w-lg text-center">
+      <div className="w-full max-w-lg text-center px-8">
         <h1 className="text-3xl font-bold mb-4">Welcome to Resume generator</h1>
         <RedirectForm />
         <p className="mt-4">As a software startup owner I really enjoy when people send us their resumes and they include their github account so we can see tangible work they have done.</p>
